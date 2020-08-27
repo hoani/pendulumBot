15839d405981a16fc8fcc5251eee9f2f6e81ecb0
Hoani <hoani.bryson@gmail.com>
2020-08-27 12:18:20 +1200

    Add start PWM to motor pair

--- core/motorPair.py
+++ core/motorPair.py
@@ -1,7 +1,14 @@
+import numpy as np
+
+
 class MotorPair:
-    def __init__(self, left, right):
+    def __init__(self, left, right, left_start=0.1, right_start=0.1):
         self.left = left
         self.right = right
+        self.l0 = left_start
+        self.r0 = right_start
+        self.l_scale = 1.0 / (1.0 + left_start)
+        self.r_scale = 1.0 / (1.0 + right_start)
 
     def forward(self, speed):
         self.move(speed, speed)
@@ -20,6 +27,8 @@ class MotorPair:
         self.right.stop()
 
     def move(self, left, right):
+        left = np.sign(left) * (abs(left) + self.l0) * self.l_scale
+        right = np.sign(right) * (abs(right) + self.r0) * self.r_scale
         self.left.run(-left)
         self.right.run(right)
 
